e116fbe9b5aa69184a11cbafaf70f2d8ac132f42
apidotmy <apid9755@gmail.com>
2023-08-17 04:02:34 +0800

    release

--- Database/tengah_cuba_insert_semua.py
+++ Database/tengah_cuba_insert_semua.py
@@ -5,26 +5,46 @@ import json
 import base64
 from datetime import datetime
 from colorama import init, Fore
+
 init(autoreset=True)
 
 EXCLUDED_DATABASES = ['master', 'tempdb', 'model', 'msdb']
 
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, datetime):
-            return obj.strftime('%Y-%m-%d %H:%M:%S')
+        if isinstance(obj, bytes):
+            return obj.decode('utf-8')
+        elif isinstance(obj, datetime): 
+            return obj.isoformat()
         return super().default(obj)
 
 data_type_mapping = {
     'int': 'INT',
+    'bigint': 'BIGINT',
+    'smallint': 'SMALLINT',
+    'tinyint': 'TINYINT',
+    'float': 'FLOAT',
+    'real': 'REAL',
+    'decimal': 'DECIMAL',
+    'numeric': 'NUMERIC',
+    'money': 'DECIMAL',  # You might need to adjust this based on MySQL precision and scale
+    'smallmoney': 'DECIMAL',  # You might need to adjust this based on MySQL precision and scale
+    'char': 'CHAR',
+    'varchar': 'VARCHAR',
     'nvarchar': 'VARCHAR',
+    'text': 'TEXT',
+    'ntext': 'TEXT',
+    'date': 'DATE',
     'datetime': 'DATETIME',
+    'datetime2': 'DATETIME',
+    'datetimeoffset': 'DATETIME',
+    'smalldatetime': 'DATETIME',
+    'time': 'TIME',
+    'timestamp': 'TIMESTAMP',
     'binary': 'BINARY',
     'varbinary': 'VARBINARY',
-    'text': 'TEXT',
-    'blob': 'BLOB',
-    'version_number': 'INT',  # Use a different column name here
-    # Add more datatype mappings as needed
+    'image': 'BLOB',
+    # Add more data type mappings as needed
 }
 
 def clear():
@@ -44,6 +64,7 @@ def main():
     for database in databases:
         database_name = database['name']
         if database_name not in EXCLUDED_DATABASES:
+            print(f"{Fore.YELLOW}Processing database: {database_name}")
             process_database(mysql_server, database_name, results)
 
     save_results_to_json(results)
@@ -56,6 +77,7 @@ def fetch_databases(sql_server):
     return databases
 
 def process_database(mysql_server, database_name, results):
+    print(f"{Fore.YELLOW}Processing tables in database: {database_name}")
     create_database(mysql_server, database_name)
     sql_server = SQLSERVER(dbname=database_name)
     table_info = get_table_info(sql_server)
@@ -67,6 +89,7 @@ def process_database(mysql_server, database_name, results):
             "table_info": table_info,
             "table_data": table_data
         }
+        print(f"{Fore.GREEN}Processed database: {database_name}")
 
 def create_database(mysql_server, database_name):
     create_db_query = f"CREATE DATABASE IF NOT EXISTS {database_name};"
@@ -86,13 +109,14 @@ def get_table_info(database_connection):
     return table_info
 
 def get_table_columns(database_connection, table_name):
-    query = f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}';"
+    query = f"SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}';"
     columns = database_connection.query(query)
     column_info = {}
 
     for column in columns:
         column_name = column['COLUMN_NAME']
-        column_info[column_name] = None
+        data_type = column['DATA_TYPE']
+        column_info[column_name] = data_type
 
     return column_info
 
@@ -121,9 +145,18 @@ def get_table_data(database_connection):
     return table_data
 
 def generate_create_table_query(table_name, columns):
-    column_definitions = [f"`{column_name}` {data_type}" for column_name, data_type in columns.items()]
+    column_definitions = []
+    for column_name, data_type in columns.items():
+        mapped_data_type = data_type_mapping.get(data_type, 'TEXT')
+        column_definitions.append(f"`{column_name}` {mapped_data_type}")
     columns_definition = ", ".join(column_definitions)
     return f"CREATE TABLE IF NOT EXISTS `{table_name}` ({columns_definition});"
+
+def generate_insert_query(table_name, row):
+    columns = ', '.join([f'[{column}]' for column in row.keys()])
+    placeholders = ', '.join(['?'] * len(row))
+    query = f"INSERT INTO [{table_name}] ({columns}) VALUES ({placeholders});"
+    return query
 
 def save_results_to_json(results):
     json_output = json.dumps(results, indent=4, cls=CustomJSONEncoder, ensure_ascii=False)
@@ -140,7 +173,7 @@ def save_results_to_json(results):
         mysql_server.execute(sql=create_db_query)
         mysql_server.commit()
 
-        use_db_query = f"USE {database_name};"
+        use_db_query = f"USE `{database_name}`;"
         mysql_server.execute(sql=use_db_query)
         mysql_server.commit()
 
@@ -150,19 +183,19 @@ def save_results_to_json(results):
                 mysql_server.execute(create_table_query)
                 mysql_server.commit()
 
-<<<<<<< HEAD:Database/insert.py
                 for row in data["table_data"][table_name]:
                     insert_query = generate_insert_query(table_name, row)
                     values = tuple(row.values())
                     mysql_server.execute(sql=insert_query, args=values)
                     mysql_server.commit()
+                    print(Fore.CYAN + f"Inserted row into table {table_name}: {row}")
 
     mysql_server.close()
 
 if __name__ == "__main__":
     clear()
+    mysql_server = MYSQL()
+    print(Fore.MAGENTA + "Starting data migration process...")
     main()
-=======
-    finally:
-        mysql_server.close()
->>>>>>> 3d37c91c22e19d8e8ac07ec96fd0ec232cc0fe5e:Database/tengah_cuba_insert_semua.py
+    print(Fore.MAGENTA + "Data migration process completed.")
+    mysql_server.close()
